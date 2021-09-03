2eeea7a04bc56f7b9d76a56248d347fec6feef41
audubilly <audubilly89@gmail.com>
2021-09-03 20:02:10 +0100

    resource, model and testing the catalog Api

---
+++ src/main/java/io/billy/microservices/moviecatalogservice/models/CatalogDetails.java
@@ -0,0 +1,38 @@
+package io.billy.microservices.moviecatalogservice.models;
+
+public class CatalogDetails {
+
+    private String name;
+    private String desc;
+    private int rating;
+
+    public CatalogDetails(String name, String desc, int rating) {
+        this.name = name;
+        this.desc = desc;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
